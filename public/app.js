19c754599a3aca9be925af050590cfca71eb300a
Jaysen <jayhip16@gmail.com>
2022-12-27 10:09:03 -0500

    Added Google as Sign-In Method; Changed all import statements to Version 9(Modular);Added sections for when Signed-In and when Signed-Out.

--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import {getAnalytics} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {GoogleAuthProvider} from "firebase/auth";
+
 
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
-import { } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+import {getAuth} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -17,10 +19,22 @@ const firebaseConfig = {
     appId: "1:316284802587:web:e52d085659cf4689ee4abe",
     measurementId: "G-PY4KFT5WVV"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 console.log(app);
 console.log(analytics);
+
+const auth = getAuth(app);
+
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+
+const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
+
+const userDetails = document.getElementById("userDetails");
+
+const provider = new GoogleAuthProvider();
+
