9e895c40b809e004add640ed2e7751cfb91a5c4f
Jaysen <jayhip16@gmail.com>
2022-12-31 11:25:38 -0500

    Added image upload

--- public/app.js
+++ public/app.js
@@ -35,58 +35,19 @@ auth.onAuthStateChanged(user => {
 
 
 
-///// Firestore /////
+function uploadFile(files) {
 
-const db = firebase.firestore();
+    const storage = firebase.storage();
+    const storageRef = storage.ref();
+    const bradRef = storageRef.child("images/BradPo.jpg");
 
-const createThing = document.getElementById('createThing');
-const thingsList = document.getElementById('thingsList');
+    const file = files.item(0);
 
+    const task = bradRef.put(file)
 
-let thingsRef;
-let unsubscribe;
-
-auth.onAuthStateChanged(user => {
-
-    if (user) {
-
-        // Database Reference
-        thingsRef = db.collection('things')
-
-        createThing.onclick = () => {
-
-            const { serverTimestamp } = firebase.firestore.FieldValue;
-
-            thingsRef.add({
-                uid: user.uid,
-                name: faker.commerce.productName(),
-                createdAt: serverTimestamp()
-            });
-        }
-
-
-        // Query
-        unsubscribe = thingsRef
-            .where('uid', '==', user.uid)
-            .orderBy('createdAt') // Requires a query
-            .onSnapshot(querySnapshot => {
-
-                // Map results to an array of li elements
-
-                const items = querySnapshot.docs.map(doc => {
-
-                    return `<li>${doc.data().name}</li>`
-
-                });
-
-                thingsList.innerHTML = items.join('');
-
-            });
-
-
-
-    } else {
-        // Unsubscribe when the user signs out
-        unsubscribe && unsubscribe();
-    }
-});
+    task.then(snapshot => {
+        console.log(snapshot);
+        const url = snapshot.downloadURL
+        document.querySelector("#imgUpload").setAttribute('src', url)
+    })
+}
